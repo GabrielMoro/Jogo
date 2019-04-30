a25f70f7604feb34530e2ed35249bde8407b2b44
gabrielmoro <gabrielhmoro@gmail.com>
2019-04-30 13:40:54 -0300

    Funcionando?

--- T1.js
+++ T1.js
@@ -69660,7 +69660,7 @@ var lime_utils_AssetCache = function() {
 	this.audio = new haxe_ds_StringMap();
 	this.font = new haxe_ds_StringMap();
 	this.image = new haxe_ds_StringMap();
-	this.version = 529027;
+	this.version = 114020;
 };
 $hxClasses["lime.utils.AssetCache"] = lime_utils_AssetCache;
 lime_utils_AssetCache.__name__ = ["lime","utils","AssetCache"];
